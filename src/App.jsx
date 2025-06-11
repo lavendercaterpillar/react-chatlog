42bf51475cb677ac4be42acaa2f29fd7e3b6095e
Ellie Alavi <elhamalavi.f@gmail.com>
2025-06-11 18:35:28 -0400

    Wave 01 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 
+const Chat = {
+  id: 1,
+  sender:'Vladimir',
+  body:'why are you arguing with me',
+  timeStamp:'2018-05-29T22:49:06+00:00',
+  liked: false
+};
 const App = () => {
   return (
     <div id="App">
@@ -7,8 +15,9 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {/* Wave 01: Render one ChatEntry component */}
+        <span>{<ChatEntry chats={Chat} />}</span>
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,20 +1,32 @@
 import './ChatEntry.css';
+import PropTypes from 'prop-types';
+// import TimeStamp from './TimeStamp';
+// import { useState } from 'react';
 
-const ChatEntry = () => {
+
+// const ChatEntry = ({id, sender, body, timeStamp, liked}) => {
+const ChatEntry = ({chats}) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{chats.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{chats.body}</p>
+        <p className="entry-time">{chats.timeStamp}</p>
         <button className="like">🤍</button>
+        {/* <button className="like">{chats.liked ? '❤️' : '🤍'}</button> */}
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  // Fill with correct proptypes
+  chats:PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    sender: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    timeStamp: PropTypes.string.isRequired,
+    liked: PropTypes.bool.isRequired,
+  }).isRequired,
 };
 
 export default ChatEntry;
